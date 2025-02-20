851e7b485968bcb7779ae747516eaa73e9bb0bb2
sophiestrobl <sstrobl@tulane.edu>
2025-02-20 00:47:55 +0000

    commit

--- test_main.py
+++ test_main.py
@@ -6,3 +6,6 @@ from main import *
 def test_multiply():
     assert quadratic_multiply(BinaryNumber(3), BinaryNumber(3)) == 3*3
     assert quadratic_multiply(BinaryNumber(4), BinaryNumber(5)) == 4*5
+
+def test_quadratic_multiply():
+    assert quadratic_multiply(BinaryNumber(8), BinaryNumber(8)) == 8*8
